3bc56175fb785e7e21c63c26b09de9323c8d5cb7
Deepa Verma <deverma@synapseco.com>
2018-07-21 16:56:27 +0530

    update payment

--- registrationdeposit.php
+++ registrationdeposit.php
@@ -335,7 +335,43 @@ function registrationdeposit_civicrm_postProcess($formName, &$form) {
   }
   
   if ($formName == 'CRM_Event_Form_Registration_Confirm') {
-    $params = $form->_values['params'];
+    $params = $form->getVar('_params');
+    $contribution_id = CRM_Utils_Array::value('contributionID', $params);
+    $payment_processor_id = CRM_Utils_Array::value('payment_processor_id', $params);
+    $trxn_id = CRM_Utils_Array::value('trxn_id', $params);
+    $status = CRM_Core_PseudoConstant::getKey('CRM_Contribute_DAO_Contribution', 'contribution_status_id', 'Partially paid');
+    $total_amount = CRM_Utils_Array::value('amount', $params);
+    $min_amount = CRM_Utils_Array::value('min_amount', $params);
+    if( $min_amount && $total_amount > $min_amount) {
+      $params = array(
+        'total_amount' => $total_amount,
+        'partial_payment_total' => $total_amount,
+        'partial_amount_to_pay' => $min_amount,
+        'contribution_status_id' => $status,
+      );
+      $contribution = new CRM_Contribute_BAO_Contribution();
+      $contribution->copyValues($params);
+      $contribution->id = $contribution_id;
+      $result = $contribution->save();
+    }
+    $payment = civicrm_api3('Payment', 'get', array('contribution_id' => $contribution_id));
+    $paymentParams = array();
+    if( !empty($payment['values'] ) ){
+      $paymentID = CRM_Utils_Array::value('id', $payment);
+      $payment_params = $payment['values'][$paymentID];
+      foreach ($payment_params as $key=>$value) {
+        $paymentParams[$key] = $value;
+      }
+    }
+    $paymentParams['net_amount'] = $min_amount;
+    $paymentParams['fee_amount'] = 0;
+    $paymentParams['is_payment'] = 0;
+    $updatedPayment = civicrm_api3('FinancialTrxn', 'create', $paymentParams);
+    
+    unset($paymentParams['id']);
+    $paymentParams['total_amount'] = $min_amount;
+    $paymentParams['is_payment'] = 1;
+    $createdPayment = civicrm_api3('FinancialTrxn', 'create', $paymentParams);
   }
 }
 
